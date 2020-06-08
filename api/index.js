50c81ff4d32a446f15cad3c90bdeb69b290393a9
Felipe <luisfelipe7lf7@hotmail.com>
2020-06-08 12:08:05 -0500

    Se crea la carpeta .env donde se guarda la constante de la  db de mongo, se configura el archivo index con las conf de la base de datos y se prueba su conexion donde esta funcioando correctamente, se agrega el now secrets add mongodb-uri y la url en string para conectar la base de datos serverless

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 const express = require('express') //importar express
+const mongoose = require('mongoose')
 const app = express ()  // crear app
 
+mongoose.connect(process.env.MONGODB_URI)
+
+const Users = mongoose.model('User', new mongoose.Schema({ name: String }))
+
+Users.create({name: 'Luis Felipe'})
+
 app.get('*',(req, res) => {
-    console.log('Hola mundo, mi primera app serverless')
-    res.send({ mensaje: "felipe" })
+    Users.find()
+    .then(x => res.send(x))
 })  //una funcion que contiene request lo que envia el cliente, el objeto que se va a utilizar para contestar
 
 module.exports = app    
