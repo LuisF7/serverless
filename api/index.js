7265e43c55c8fe897e6568a3151f0fb8e4aa201b
Felipe <luisfelipe7lf7@hotmail.com>
2020-06-09 11:42:21 -0500

    Se agregan los modelos Order y Meals a nuestro servidor serverless

--- api/index.js
+++ api/index.js
@@ -2,15 +2,11 @@ const express = require('express') //importar express
 const mongoose = require('mongoose')
 const app = express ()  // crear app
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true, useUnifiedTopology: true })
 
-const Users = mongoose.model('User', new mongoose.Schema({ name: String }))
-
-Users.create({name: 'Luis Felipe'})
 
 app.get('*',(req, res) => {
-    Users.find()
-    .then(x => res.send(x))
+    res.send('Hola mundo')
 })  //una funcion que contiene request lo que envia el cliente, el objeto que se va a utilizar para contestar
 
 module.exports = app    
